5497fd5dfb552687b808abf9d3b049da6c470774
Ryan Briones <ryan.briones@brionesandco.com>
2013-11-12 21:53:28 -0600

    add searches and ability to create/show search

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,14 @@
 get '/' do
-  # Look in app/views/index.erb
   erb :index
+end
+
+post "/searches" do
+  @search = Search.create!(params[:search])
+  redirect to("/searches/#{@search.id}")
+end
+
+get "/searches/:id" do
+  @search = Search.find(params[:id])
+
+  erb :search_show
 end
---
+++ db/migrate/20131112213900_create_searches.rb
@@ -0,0 +1,10 @@
+class CreateSearches < ActiveRecord::Migration
+  def change
+    create_table :searches do |t|
+      t.string :keywords
+      t.boolean :on, default: true
+
+      t.timestamps
+    end
+  end
+end
