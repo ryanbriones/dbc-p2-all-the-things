231d4c7cfa82fb8c23851a69c33aca39d29654d4
Clark Kampfe <clark.kampfe@gmail.com>
2013-11-15 12:16:17 -0600

    remove procedural code

--- public/js/realtime.js
+++ public/js/realtime.js
@@ -61,33 +61,3 @@ function getTweets() {
 $(document).ready(function() {
   getTweets();
 });
-
-// // via http://stackoverflow.com/a/5052661
-//   (function periodicTimer() {
-//     var url = "/searches/" + searchId + ".json";
-//     var data = {
-//       since: lastUpdated
-//     }
-//
-//     $.get(url, data, function(response) {
-//       if(response.stop) {
-//         window.location.href = "/searches/" + searchId;
-//       } else {
-//         lastUpdated = response.timestamp;
-//
-//         for(var i = 0; i < response.tweets.length; i++) {
-//           var newTweet = response.tweets[i].tweet;
-//           var newTweetHTML = $("#tweet_template .tweet").clone();
-//           newTweetHTML.hide();
-//           newTweetHTML.find(".screen_name").html(newTweet.screen_name);
-//           newTweetHTML.find(".text").html(newTweet.text);
-//           newTweetHTML.find(".tweeted_at").html(newTweet.tweeted_at);
-//
-//           $('#tweets').prepend(newTweetHTML);
-//           newTweetHTML.delay(100 * i).fadeIn(500);
-//         }
-//
-//         setTimeout(periodicTimer, 5000);
-//       }
-//     });
-//   })();
